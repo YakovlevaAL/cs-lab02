0c0811818677a7dd180f275118f35a876f2cd7dd
Alice (YakovlevaAL) <alice@example.com>
2021-04-11 23:41:45 +0300

    code: добавлен вывод максимума

--- main.cpp
+++ main.cpp
@@ -12,5 +12,10 @@ int main()
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n';
 
+     if (a > b)
+        cout << "max = " << a << '\n';
+     else
+        cout << "max = " << b << '\n';
+
     return 0;
 }
